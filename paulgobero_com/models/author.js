06c13d18127e7053a44317543694b3301b254884
paulgl <paul@paulgobero.com>
2022-11-20 00:45:11 +0300

    Added trim attribute

--- paulgobero_com/models/author.js
+++ paulgobero_com/models/author.js
@@ -7,18 +7,18 @@ const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
     name: {
-	first: { type: String, required: [true, 'Please insert first name'], maxLength: 50 },
-	middle:  { type: String, maxLength: 50 },
-	last: { type: String, required: [true, 'Please insert last name'], maxLength: 50 }
+	first: { type: String, required: [true, 'Please insert first name'], maxLength: 50, trim: true },
+	middle:  { type: String, maxLength: 50, trim: true },
+	last: { type: String, required: [true, 'Please insert last name'], maxLength: 50, trim: true }
     },
     contact: {
 	phoneNumber: {
-	    work: { type: String },
-	    home: { type: String }
+	    work: { type: String, trim: true },
+	    home: { type: String, trim: true }
 	}
     },
-    email: { type: String, required: [ true, 'Please enter your email address' ], lowercase: true },
-    personal_website: { type: String }
+    email: { type: String, required: [ true, 'Please enter your email address' ], lowercase: true, trim: true },
+    personal_website: { type: String, trim: true }
 }, { timestamps: true });
 
 //define the virtual property
