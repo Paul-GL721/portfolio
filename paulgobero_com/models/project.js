e51a50bca7b7cfa4c39a82753e4a10099d71657a
paulgl <paul@paulgobero.com>
2022-11-20 17:17:26 +0300

    Added project model

---
+++ paulgobero_com/models/project.js
@@ -0,0 +1,27 @@
+
+//####### Create project module from schema ######
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//create schema
+const ProjectSchema = new Schema({
+    ptitle: { type: String, required: true },
+    psummary: { type: String, required: true },
+    problemStatement: { type: String, required: true },
+    solutin: { type: String },
+    skill: { type: Schema.Types.ObjectId, ref: "Skill", required: true },
+    role: { type: String, required: true },
+    githubUrl: { type: String },
+    skill: [{ type: Schema.Types.ObjectId, ref: "Skill", required: true }],
+    author: [{ type: Schema.Types.ObjectId, ref: "Author" }],
+    specialisation: [{ tyepe: Schema.Types.ObjectId, ref: "Specialisation", required: true }]
+}, {timestamps: true });
+
+//Add virtual property
+ProjectSchema.virtual("url").get(function() {
+    return `/paulgobero_com/project/${this._id}`;
+});
+
+//export model
+module.exports = mongoose.model("Project", ProjectSchema);
