bd78b3c97bdfdbd6324ba02514b34ed6fd7cebb8
Paul G Lwanga <lwangapaul23@gmail.com>
2022-11-22 23:54:57 +0300

    Edited path

--- paulgobero_com/jenkins-scripts/build-step.sh
+++ paulgobero_com/jenkins-scripts/build-step.sh
@@ -6,11 +6,7 @@ docker version
 echo Building the docker container
 
 #  With 'sed commad' replace the jversion variable with the jenkins build number
-sed  -i -e 's/JVERSION/$VERSION/g' docker-compose.yml
+sed  -i -e 's/JVERSION/$VERSION/g' ./${BASE_DIRECTORY}/docker-compose.yml
 
 ## With docker-compose build a taged image
 docker-compose build
-
-
-
-## Push the image to docker hub
