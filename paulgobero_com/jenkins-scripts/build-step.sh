eccc98dffa6fa825d6e30ce4126990798ab36351
Paul G Lwanga <lwangapaul23@gmail.com>
2022-11-23 13:02:23 +0300

    Execute docker builds as user

--- paulgobero_com/jenkins-scripts/build-step.sh
+++ paulgobero_com/jenkins-scripts/build-step.sh
@@ -5,11 +5,23 @@ docker-compose --version
 docker version
 echo Building the docker container
 
-#  With 'sed commad' replace the jversion variable with the jenkins build number
+# With 'sed commad' replace the jversion variable with the jenkins build number
 sed  -i -e 's/JVERSION/$VERSION/g' ./$BASE_DIRECTORY/docker-compose.yml
 
 ## With docker-compose build a taged image
 docker-compose -f ./$BASE_DIRECTORY/docker-compose.yml build
+
+# Switch user and login and push image to docker hub 
+sudo su
+apt-get update
+su ubuntu <<HERE
+whoami
+echo usr=$USER
+
+echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
+
+docker push paulgl721/nodejs-portfolio:$VERSION
+HERE
 
 #sh '''sed  -i -e 's/JVERSION/$VERSION/g' ./${BASE_DIRECTORY}/docker-compose.yml'''
 #sh '''docker-compose -f ./${BASE_DIRECTORY}/docker-compose.yml build'''
