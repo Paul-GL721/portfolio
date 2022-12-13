c11e7f98b82a98b15e398c7c586f19e555036f09
Paul G Lwanga <lwangapaul23@gmail.com>
2022-12-13 17:50:35 +0300

    Testing multiple prs

--- paulgobero_com/jenkins-scripts/pr-branches.sh
+++ paulgobero_com/jenkins-scripts/pr-branches.sh
@@ -12,7 +12,9 @@ git checkout -b tmpstagingV$VERSION
 
 #If any of these files or folder changes in the development branch
 #merge them into the staging branch
-git checkout origin/staging paulgobero_com/bin paulgobero_com/configs paulgobero_com/models paulgobero_com/mongodb/Dockerfile paulgobero_com/public paulgobero_com/routes paulgobero_com/views paulgobero_com/app.js paulgobero_com/Jenkinsfile paulgobero_com/package-lock.json paulgobero_com/package.json
+#git checkout origin/staging paulgobero_com/bin paulgobero_com/configs paulgobero_com/models paulgobero_com/mongodb/Dockerfile paulgobero_com/public paulgobero_com/routes paulgobero_com/views paulgobero_com/app.js paulgobero_com/Jenkinsfile paulgobero_com/package-lock.json paulgobero_com/package.json
+
+git add origin/staging paulgobero_com/bin paulgobero_com/configs paulgobero_com/models paulgobero_com/mongodb/Dockerfile paulgobero_com/public paulgobero_com/routes paulgobero_com/views paulgobero_com/app.js paulgobero_com/Jenkinsfile paulgobero_com/package-lock.json paulgobero_com/package.json
 
 git push --set-upstream origin tmpstagingV$VERSION
 
