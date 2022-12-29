79085de29069e81a8467853ee1310c66cace86e5
Paul <paul@paulgobero.com>
2022-12-29 23:59:33 +0300

    Added website controllers

---
+++ paulgobero_com/controllers/authorController.js
@@ -0,0 +1,48 @@
+//author model controller
+
+const Author = require("../models/author");
+
+//Display home website page
+exports.index = (req, res) => {
+	res.send("NOT IMPLEMENTED: Website Home page");
+};
+
+//Display a list of authors
+exports.author_list = (req, res) => {
+	res.send("NOT IMPLEMENTED: Author list");
+};
+
+//Display details of specific author
+exports.author_detail = (req, res) => {
+	res.send(`NOT IMPLEMENTED: Author details: ${req.params.id}`);
+}
+
+//Display author create form on Get
+exports.author_create_get = (req, res) => {
+	res.send("NOT IMPLEMENTED: Author create get");
+}
+
+//Display author create form on Post
+exports.author_create_post = (req, res) => {
+	res.send("NOT IMPLEMENTED: Author create post");
+}
+
+//Display author delete form on Get
+exports.author_delete_get = (req, res) => {
+	res.send("NOT IMPLEMENTED: Author delete get");
+}
+
+//Display author delete form on Post
+exports.author_delete_post = (req, res) => {
+	res.send("NOT IMPLEMENTED: Author delete post");
+}
+
+//Display author update form on Get
+exports.author_update_get = (req, res) => {
+	res.send("NOT IMPLEMENTED: Author update get");
+}
+
+//Display author update form on Post
+exports.author_update_post = (req, res) => {
+	res.send("NOT IMPLEMENTED: Author update post");
+}
