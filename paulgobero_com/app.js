dfb4fa74e295cf412bb796d27ee00b2401774526
server2 <paul@paulgobero.com>
2022-12-29 21:05:39 +0000

    Merged from staging branch: Build version 0.1.195

--- paulgobero_com/app.js
+++ paulgobero_com/app.js
@@ -15,6 +15,7 @@ nunjucks.configure('views', {
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var websiteRouter = require('./routes/website');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/website', websiteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
