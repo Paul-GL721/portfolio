63a628dc98963179d490d51b917b371f3baa42df
paulpc2 <paulpc2@paulgobero.com>
2023-01-16 21:42:32 +0300

    changed db connection path

--- paulgobero_com/app.js
+++ paulgobero_com/app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var nunjucks = require('nunjucks');
-var db = require('./configs/loadb');
+var  database_connection = require('./configs/loadb');
+database_connection();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
