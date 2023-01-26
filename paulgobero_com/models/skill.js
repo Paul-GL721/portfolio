a0da61a97f294465f651991adf7c8c2e2f08b132
paulpc2 <paulpc2@paulgobero.com>
2023-01-26 21:11:42 +0300

    Renamed image and added url to s3 bucket

--- paulgobero_com/models/skill.js
+++ paulgobero_com/models/skill.js
@@ -3,13 +3,14 @@
 //############# Create the skills model from schema ##########
 
 const mongoose = require("mongoose");
-const Schema = mongoose.schema;
+const Schema = mongoose.Schema;
 
 //create the schema
 const SkillSchema = new Schema({
     name: { type: String, required: true, maxLength: 30 },
-    description: type: String,
-    image: { data: Buffer, contentType: String }
+    description: { type: String, required: true, maxLength: 300 },
+    imageName: { type: String, required: true },
+    imageUrl: { type: String }
 }, {timestamps: true} );
 
 //export model
