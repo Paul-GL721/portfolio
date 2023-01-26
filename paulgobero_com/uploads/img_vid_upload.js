dcb0dd9ca9635e34aa73fced18508e05c860e405
paulpc2 <paulpc2@paulgobero.com>
2023-01-26 21:13:50 +0300

    changed storage from local disk to memory

--- paulgobero_com/uploads/img_vid_upload.js
+++ paulgobero_com/uploads/img_vid_upload.js
@@ -1,18 +1,19 @@
 /* ---------------Script to upload Images and Videos--------------------- */
 
-var multer = require('multer');
-var path = require('path');
+const multer = require('multer');
 
-const uploadimgfilepath = path.join(__dirname, 'images')
+/*var path = require('path');
+const uploadimgfilepath = path.join(__dirname, 'images')*/
+const storage = multer.memoryStorage();
 
-const storage = multer.diskStorage({
+/*const storage = multer.diskStorage({
     destination: function(req, file, cb) {
       cb(null, uploadimgfilepath);
     },
     filename: function(req, file, cb) {
       cb(null, new Date().toISOString() + file.originalname);
     }
-  });
+  });*/
   
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
