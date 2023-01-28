18e4e078b03022f71cbd9fce6b6e9c67b5f7acc9
paulmac <paul@paulgobero.com>
2023-01-28 23:52:14 +0300

    Added the safer-buffer module

--- paulgobero_com/app.js
+++ paulgobero_com/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Buffer = require('safer-buffer');
 var nunjucks = require('nunjucks');
 var db = require('./configs/loadb');
 
