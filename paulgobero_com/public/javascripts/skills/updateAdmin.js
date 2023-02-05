ca6e95ce208a14409ba2455b237734e4c18e5316
paulmac <paul@paulgobero.com>
2023-02-06 00:28:15 +0300

    Removed post url bug

--- paulgobero_com/public/javascripts/skills/updateAdmin.js
+++ paulgobero_com/public/javascripts/skills/updateAdmin.js
@@ -11,9 +11,10 @@ $(document).ready(function(){
 				success: function(data){
 					console.log(data);
 					$("#skillUpdateModal").modal('show');					
-					$("#skillname").val(data.update_skill.name);
-					$("#skilldescription").val(data.update_skill.description);
-					$("div1").attr("src", data.update_skill.imageUrl);
+					$("#skillname").val(data.name);
+					$("#skilldescription").val(data.description);
+					$("#div1").attr("src", data.imageUrl);
+					
 				}
 			});
 		});
