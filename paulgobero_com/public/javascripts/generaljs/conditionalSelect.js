392e0a2045f5102f1f3aa0a91ed6b1dd947280f8
paulmac <paul@paulgobero.com>
2023-02-20 20:53:44 +0300

    updated multiple select

--- paulgobero_com/public/javascripts/generaljs/conditionalSelect.js
+++ paulgobero_com/public/javascripts/generaljs/conditionalSelect.js
@@ -2,19 +2,21 @@ $('select[data-source]').each(function() {
   var $select = $(this);
   
   $.ajax({
+    type: "GET",
     url: $select.attr('data-source'),
     dataType: "json",
-  }).then(function(options) {
-    options.map(function(option) {
+  }).then(function(datas) {
+    datas.map(function(option) {
       var $option = $('<option>');
       
       $option
         .val(option[$select.attr('data-valueKey')])
         .text(option[$select.attr('data-displayKey')]);
-      
       $select.append($option);
+      
     });
+    console.log(datas)
     //multiselct reload the options
-    $('#proskills, #projspecialisation').multiselect('rebuild');
+    $('#proskills, #projspecialisation, #projauthor').multiselect('rebuild');
   });
 });
--- paulgobero_com/public/javascripts/generaljs/multipleSelections.js
+++ paulgobero_com/public/javascripts/generaljs/multipleSelections.js
@@ -1,5 +1,5 @@
 
 $(document).ready(function() { 
      //intialise multiselect
-    $('#proskills, #projspecialisation').multiselect();  
+    $('#proskills, #projspecialisation, #projauthor').multiselect();  
 });  
