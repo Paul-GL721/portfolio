7867096e78f6e70995bd062c80da0c38794e0ebc
paulmac <paul@paulgobero.com>
2023-02-20 20:48:47 +0300

    removed the all authors api

--- paulgobero_com/routes/website.js
+++ paulgobero_com/routes/website.js
@@ -69,8 +69,6 @@ router.get("/project/api/skills", skill_controller.project_skill);
 //Get request for all project skills
 router.get("/project/api/specialisations", specialisation_controller.project_specialisations);
 
-//Get request for all author skills
-router.get("/project/api/author", author_controller.project_authors);
 				//........BLOG ROUTES.....//
 
 
