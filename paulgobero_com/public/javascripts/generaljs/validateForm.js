3af6de42c4004caa8b4555fe57e95205f62326b9
paulmac <paul@paulgobero.com>
2023-03-10 22:48:07 +0300

    Client side contact form validation

--- paulgobero_com/public/javascripts/generaljs/validateForm.js
+++ paulgobero_com/public/javascripts/generaljs/validateForm.js
@@ -20,16 +20,31 @@ $(document).ready(function() {
 		messages:{
 			contactname: {
 		      required: "I need your name to address you correctly",
-		      minlength: jQuery.validator.format("At least {0} characters required!")
+		      minlength: jQuery.validator.format("At least {3} characters required!")
 		    },
 		    contactemail: {
-		      required: "We need your email address to contact you",
+		      required: "I need your email address to email you back",
 		      email: "Your email address must be in the format of name@domain.com"
 		    },
 		    contactmessage: {
 		      required: "Please type your message here",
-		      minlength: jQuery.validator.format("At least {0} characters required!")
+		      minlength: jQuery.validator.format("At least {3} characters required!")
 		    }
-		}		
+		},
+		submitHandler: function(form,e) {
+			e.preventDefault();
+			const formsdata = new FormData($("#contactForm")[0]);
+			/*console.log("formsdata is");
+			console.log(formsdata);
+			formsdata.has("contactmessage");
+			// Display the values
+			for (const value of formsdata.values()) {
+				console.log(value);
+			}*/
+			form.submit(function(){
+				alert("Message successfully sent");
+
+			});
+		}
 	});
 });
