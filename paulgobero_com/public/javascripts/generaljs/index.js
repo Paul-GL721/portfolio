7f3d0e527ae593b8fae74ebb029e58cd6c0d08aa
paulmac <paul@paulgobero.com>
2023-05-13 01:18:44 +0300

    Display message when contact form is submitted

--- paulgobero_com/public/javascripts/generaljs/index.js
+++ paulgobero_com/public/javascripts/generaljs/index.js
@@ -3,4 +3,21 @@ $(document).ready(function() {
       var projectId = $(this).data('project-id');
       $('#projectmodal-' + projectId).modal()
     });
+
+    $('#contactForm').on('submit', function(event) {
+      event.preventDefault();
+      $.ajax({
+        url: "/",
+        method: "POST",
+        data: $(this).serialize(),
+        success: function(response) {
+          $("#contactForm")[0].reset();
+          $('#contactmodal').modal('show');
+        },
+        error: function(error) {
+          console.log(error);
+          alert("An error occured while sending the message, please try again later")
+        }
+      });
+    });  
 });
