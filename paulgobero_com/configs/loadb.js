e87bccec2bbdf8b2bddaf3e50c54cbea0fb69803
paulmac <paul@paulgobero.com>
2023-05-13 01:16:18 +0300

    use try and catch with db connection

--- paulgobero_com/configs/loadb.js
+++ paulgobero_com/configs/loadb.js
@@ -1,23 +1,18 @@
 //####### Connect mongodb to nodejs #####
 
-const database_connection = async () => {
-   //import required modules and variables
-   const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = require('./config');
+const database_connection = async (db_name, db_user, db_passwd, db_host, db_port) => {
+   //import required modules
    const mongoose = require('mongoose');
 
    //set mongoose connection
-   const mongoDBurl = 'mongodb://'+DB_USER+':'+DB_PASSWORD+'@'+DB_HOST+':'+DB_PORT+'/'+DB_NAME+'?authSource=admin';
-   
-   await mongoose.connect(mongoDBurl, { useNewUrlParser: true, useUnifiedTopology: true },
-      (err) => {
-         if (err) {
-            console.error('FAILED TO CONNECT');
-            console.error(err);
-         } else {
-            console.log('CONNECTED TO MONGODB');
-         }
-      }
-   ) 
+   const mongoDBurl = `mongodb://${db_user}:${db_passwd}@${db_host}:${db_port}/${db_name}?authSource=admin`;
+   try {
+      await mongoose.connect(mongoDBurl, { useNewUrlParser: true, useUnifiedTopology: true }); 
+      console.log('SUCCESSFULLY CONNECTED TO MONGODB');
+   } catch (error) {
+      console.error('FAILED TO CONNECT');
+      console.error('Error connecting to MongoDB', error);
+   }
 };
 module.exports = database_connection;
 
