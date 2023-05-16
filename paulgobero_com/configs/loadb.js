55081e1ebe130da157c082baf21db63692e7fd94
paulmac <paul@paulgobero.com>
2023-05-17 01:22:01 +0300

    Test database connection function before making the connection

--- paulgobero_com/configs/loadb.js
+++ paulgobero_com/configs/loadb.js
@@ -1,17 +1,22 @@
 //####### Connect mongodb to nodejs #####
 
+//import required modules
+const mongoose = require('mongoose');
+let db = null;
+
 const database_connection = async (db_name, db_user, db_passwd, db_host, db_port) => {
-   //import required modules
-   const mongoose = require('mongoose');
 
    //set mongoose connection
    const mongoDBurl = `mongodb://${db_user}:${db_passwd}@${db_host}:${db_port}/${db_name}?authSource=admin`;
    try {
-      await mongoose.connect(mongoDBurl, { useNewUrlParser: true, useUnifiedTopology: true }); 
+      if (!db) {
+         db = await mongoose.connect(mongoDBurl, { useNewUrlParser: true, useUnifiedTopology: true }); 
+      }
       console.log('SUCCESSFULLY CONNECTED TO MONGODB');
    } catch (error) {
       console.error('FAILED TO CONNECT');
       console.error('Error connecting to MongoDB', error);
+      process.exit(1);
    }
 };
 module.exports = database_connection;
