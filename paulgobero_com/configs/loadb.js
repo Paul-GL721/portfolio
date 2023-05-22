68cbe8ce839ebe10f7027f455ce1df837dfeb0c6
paulmac <paul@paulgobero.com>
2023-05-23 01:32:31 +0300

    increase connection to test db connection

--- paulgobero_com/configs/loadb.js
+++ paulgobero_com/configs/loadb.js
@@ -5,18 +5,25 @@ const mongoose = require('mongoose');
 let db = null;
 
 const database_connection = async (db_name, db_user, db_passwd, db_host, db_port) => {
+    // Set mongoose connection options
+   const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 60000, // Increase the timeout to 6000 seconds (adjust as needed)
+   };
 
    //set mongoose connection
    const mongoDBurl = `mongodb://${db_user}:${db_passwd}@${db_host}:${db_port}/${db_name}?authSource=admin`;
    try {
       if (!db) {
-         db = await mongoose.connect(mongoDBurl, { useNewUrlParser: true, useUnifiedTopology: true }); 
+         db = await mongoose.connect(mongoDBurl, options); 
       }
       console.log('SUCCESSFULLY CONNECTED TO MONGODB');
+      return true; // Connection successful
    } catch (error) {
       console.error('FAILED TO CONNECT');
       console.error('Error connecting to MongoDB', error);
-      process.exit(1);
+      return false; // Connection failed
    }
 };
 module.exports = database_connection;
