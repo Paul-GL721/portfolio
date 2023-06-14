494c9c78b485c7ab34f6e94922134c383dd8b5d5
paulmac <paul@paulgobero.com>
2023-06-14 22:41:08 +0300

    Added reusable functions to login

--- paulgobero_com/controllers/loginController.js
+++ paulgobero_com/controllers/loginController.js
@@ -5,59 +5,9 @@ const jwt = require("jsonwebtoken");
 const {  S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const { BUCKET_NAME, BUCKET_REGION, ACCESS_KEY, SECRET_ACCESS_KEY, AUTH_SECRET_KEY } = require('../configs/config');
+const controllerUtils = require("../utils/controllerUtils");
 
-//s3 bucket connection parameters
-const s3Client = new S3Client({
-	region: BUCKET_REGION,
-	credentials: {
-		accessKeyId: ACCESS_KEY,
-		secretAccessKey: SECRET_ACCESS_KEY
-	}
-});
-
-let brandname
-async function getBrandName() {
-	Author.findOne({ authorStatus: 'owner' })
-	  .then(brand => {
-		// Do something with the brand
-		
-			if (err){
-				console.log("There was an error in retrieving the brand name");
-				console.log(err);
-			} else if (!brand) {
-				console.log("No brand");
-			} else if(brand.brandName === null || brand.brandName === undefined) {
-				console.log("No brand name");
-				brandname = brand.name.first + brand.name.last;
-			} else {
-				console.log("brand name available");
-				brandname = brand.brandName;
-			}
-		
-	  })
-	  .catch(err => {
-		console.log("There was an error in retrieving the brand name");
-		console.log(err);
-	  });
-  }
-/*function getBrandName(){
-	//find the brand name for the owner
-	Author.findOne({ authorStatus: 'owner' }, function await(err, brand) {
-		if (err){
-			console.log("There was an error in retrieving the brand name");
-			console.log(err);
-		} else if (!brand) {
-			console.log("No brand");
-		} else if(brand.brandName === null || brand.brandName === undefined) {
-			console.log("No brand name");
-			brandname = brand.name.first + brand.name.last;
-		} else {
-			console.log("brand name available");
-			brandname = brand.brandName;
-		}
-	});
-	return brandname;
-}*/
+let brand
 
 //Display login page
 exports.login = async (req, res, next) => {
@@ -67,39 +17,39 @@ exports.login = async (req, res, next) => {
 let refresh_jwt_token;
 //Post login page (authentication)
 exports.login_post = async (req, res, next) => {
-	getBrandName();
-	//get the email and password from the login form
-	const username = req.body.email;
-	const passwd = req.body.password;
+	try {
+		brand = await controllerUtils.getBrandName();
+		//get the email and password from the login form
+		const username = req.body.email;
+		const passwd = req.body.password;
 
-	//find the user in the database
-	const availuser = await Author.findOne({email: username, password:passwd })
-	.exec(async (err, availresult) => {
-		if (err){
-			console.log(err);
-		}
-		//if the user is available, generate a JWT, load the admin dashboard
-		else if (availresult) {
-			const accessexpiry = new Date(Date.now() + 15 * 60 * 1000); //  10m from now
-			//create an access token for the user
-			const jwt_token = jwt.sign({ user:availresult.brandName, role:availresult.authorRole }, AUTH_SECRET_KEY, { expiresIn: '10m' } );
-			//create a refresh token for the user
-			refresh_jwt_token = jwt.sign({ user:availresult.brandName, role:availresult.authorRole }, AUTH_SECRET_KEY, { expiresIn: '1h' } );
-			//create image signed Urls
-			availresult.imageUrl = await  getSignedUrl(s3Client, new GetObjectCommand({
-				Bucket: BUCKET_NAME,
-				Key: availresult.imageName
-			}), { expiresIn: 3600})	
-			
-			res.cookie('jwtTokens',{ jwt:jwt_token, reftok:refresh_jwt_token}, { expires: accessexpiry, path: '/' });
-			//res.json({ availresult});
-			res.render("admin_dashboard", { Title: "Adminstrator Dashboard", admin_data: availresult, brandname });
-		}
-		//else the user is not available: send an error response
-		else {
-			res.json({ status: false});
-		}
-	});
+		//find the user in the database
+		const availuser = await Author.findOne({email: username, password:passwd })
+		.exec(async (err, availresult) => {
+			if (err){
+				console.log(err);
+			}
+			//if the user is available, generate a JWT, load the admin dashboard
+			else if (availresult) {
+				const accessexpiry = new Date(Date.now() + 15 * 60 * 1000); //  10m from now
+				//create an access token for the user
+				const jwt_token = jwt.sign({ user:availresult.brandName, role:availresult.authorRole }, AUTH_SECRET_KEY, { expiresIn: '10m' } );
+				//create a refresh token for the user
+				refresh_jwt_token = jwt.sign({ user:availresult.brandName, role:availresult.authorRole }, AUTH_SECRET_KEY, { expiresIn: '1h' } );
+				//create image signed Urls
+				availresult.imageUrl = await controllerUtils.signedurl( BUCKET_NAME, availresult.imageName, 3600 );  
+				res.cookie('jwtTokens',{ jwt:jwt_token, reftok:refresh_jwt_token}, { expires: accessexpiry, path: '/' });
+				//res.json({ availresult});
+				res.render("admin_dashboard", { Title: "Adminstrator Dashboard", admin_data: availresult, brand1: brand });
+			}
+			//else the user is not available: send an error response
+			else {
+				res.json({ status: false});
+			}
+		});
+	} catch {
+		console.log("An Error occurred: ", err);
+	}	
 };
 
 //Display owner signup page
