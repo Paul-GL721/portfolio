d8e156e9fdd955c530e91b19d34a942a00a98913
paulmac <paul@paulgobero.com>
2023-06-21 00:10:07 +0300

    Updated the skill delete functions with confirmation message

--- paulgobero_com/public/javascripts/skills/deleteAdmin.js
+++ paulgobero_com/public/javascripts/skills/deleteAdmin.js
@@ -1,8 +1,12 @@
 
 $(document).ready(function() {
+	//show delete confirmation modal on clicking delete button
+	$('#skillbtnDelete').on('click', function() {
+		$('#skilldeleteConfirmationModal').modal('show');
+	});
 	// body...
 	var $table = $('#skilltable')
-	var $button = $('#skillbtnDelete')
+	var $button = $('#skillconfirmDeleteButton')
 
 	$button.click(function(){
 		var skildel = $.map($table.bootstrapTable('getSelections'), function (row){
@@ -14,14 +18,21 @@ $(document).ready(function() {
 				data: {skilid:skilid},
 				success: function(data) {
 					alert('Successfully Deleted from Table!');
+					$('#skilldeleteConfirmationModal').modal('hide');
+					removeRowFromTable(skilid); // Remove the deleted row from the table
 				},
+				error: function(xhr, status, error) {
+                    alert('Error deleting row: ' + error); // Handle error case
+                }
 			});
-			return row.skills_id;
+			return skilid;
 		});
 
-		$table.bootstrapTable('remove', {
-			field: 'skills_id',
-			values: skildel
-		});
+		function removeRowFromTable(skilid) {
+			$table.bootstrapTable('remove', {
+				field: 'skills_id',
+				values: [skilid]
+			});
+		}
 	});
 });
