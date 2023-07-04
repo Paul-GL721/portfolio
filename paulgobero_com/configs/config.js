7d31d097b7487baaaa2f4b8691d8fe0b5b651336
Paul G Lwanga <lwangapaul23@gmail.com>
2023-07-04 12:27:09 +0300

    Loading env variables from docker-compose location

--- paulgobero_com/configs/config.js
+++ paulgobero_com/configs/config.js
@@ -1,7 +1,13 @@
 
 //file to load environment variables
 const dotenv = require('dotenv');
-const result = dotenv.config({ path: __dirname+'/.env.dev' });
+
+//loads env variables from local machine
+//const result = dotenv.config({ path: __dirname+'/.env.dev' });
+
+//load env variables from docker-compose
+const result = dotenv.config();
+
 if (result.error) {
     throw result.error;
 }
