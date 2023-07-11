3f8144799d387b85579cde9167ab46dbfd361de9
paulmac <paul@paulgobero.com>
2023-07-11 18:22:48 +0300

    updated file paths

--- paulgobero_com/jenkins-scripts/test-step.sh
+++ paulgobero_com/jenkins-scripts/test-step.sh
@@ -4,8 +4,8 @@ docker-compose --version
 docker version
 echo Using docker-compose to build and test
 
-docker-compose -f testdocker-compose.yml up --build -d
-docker-compose -f testdocker-compose.yml run testapp npm run test
+docker-compose -f ./paulgobero_com/testdocker-compose.yml up --build -d
+docker-compose -f ./paulgobero_com/testdocker-compose.yml run testapp npm run test
 
  
 
