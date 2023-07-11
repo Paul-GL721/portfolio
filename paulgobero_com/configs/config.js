626e36f70ebc2dce567a067062de2ff29b12a7be
server2 <paul@paulgobero.com>
2023-07-11 14:12:09 +0000

    Merged from development branch: Build version 0.1.155

--- paulgobero_com/configs/config.js
+++ paulgobero_com/configs/config.js
@@ -1,7 +1,13 @@
 
 //file to load environment variables
 const dotenv = require('dotenv');
+
+//loads env variables from local machine
 const result = dotenv.config({ path: __dirname+'/.env.dev' });
+
+//load env variables from docker-compose
+//const result = dotenv.config();
+
 if (result.error) {
     throw result.error;
 }
