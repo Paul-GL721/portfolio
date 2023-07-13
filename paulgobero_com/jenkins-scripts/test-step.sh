7631796c77dabcf6e4b6592ac167ae0ab6e0729b
paulmac <paul@paulgobero.com>
2023-07-13 23:59:52 +0300

    Testing in jenkins

--- paulgobero_com/jenkins-scripts/test-step.sh
+++ paulgobero_com/jenkins-scripts/test-step.sh
@@ -7,7 +7,7 @@ echo Using docker-compose to build and test
 npm install jest-puppeteer
 
 docker-compose -f ./paulgobero_com/testdocker-compose.yml up --build -d
-docker-compose -f ./paulgobero_com/testdocker-compose.yml exec testapp npm run test
+docker-compose -f ./paulgobero_com/testdocker-compose.yml run testapp npm run test
 
  
 
