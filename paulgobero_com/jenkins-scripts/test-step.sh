1714aa500bb3563b43c16859e67f526ab58b980c
paulmac <paul@paulgobero.com>
2023-07-14 00:41:57 +0300

    Testing in jenkins

--- paulgobero_com/jenkins-scripts/test-step.sh
+++ paulgobero_com/jenkins-scripts/test-step.sh
@@ -8,22 +8,23 @@ docker-compose --version
 docker version
 echo Using docker-compose to build and test
 
-apt-get install -y ca-certificates fonts-liberation libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
+#apt-get install -y ca-certificates fonts-liberation libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
 
-npm install jest-puppeteer
 
 echo "Starting docker-compose"
 
 docker-compose -f ./paulgobero_com/testdocker-compose.yml up --build -d
+npm install 
+npm run test
 
 echo "Waiting for docker-compose to finish"
 
-until docker-compose -f ./paulgobero_com/testdocker-compose.yml ps | grep testapp; do
-sleep 1
-done
+#until docker-compose -f ./paulgobero_com/testdocker-compose.yml ps | grep testapp; do
+#sleep 1
+#done
 
 echo "Running tests"
 
-docker-compose -f ./paulgobero_com/testdocker-compose.yml run testapp npm run test
+#docker-compose -f ./paulgobero_com/testdocker-compose.yml run testapp npm run test
 
 echo "Finished test-step.sh"
