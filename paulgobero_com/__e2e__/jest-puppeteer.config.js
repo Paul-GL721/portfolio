be466d1102f1f57139f30ca4410fae90ffd2cd4a
paulmac <paul@paulgobero.com>
2023-07-24 14:09:26 +0300

    Relocated files

--- paulgobero_com/__e2e__/jest-puppeteer.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    launch: {
-      headless: 'new',
-        args: [ '--no-sandbox', '--disable-setuid-sandbox', "--window-size=1366,768" ],
-    },
-    browser: 'chromium',
-}
