b0b0f3613a9c0e53add48a1401c8db1b30bd1289
Paul G Lwanga <lwangapaul23@gmail.com>
2023-07-28 21:46:17 +0300

    Temp config file

--- paulgobero_com/configs/config.js
+++ paulgobero_com/configs/config.js
@@ -1,7 +1,6 @@
 
 //file to load environment variables
 const dotenv = require('dotenv');
-<<<<<<< .merge_file_a15944
 
 //loads env variables from local machine
 const result = dotenv.config({ path: __dirname+'/.env.dev' });
@@ -9,9 +8,6 @@ const result = dotenv.config({ path: __dirname+'/.env.dev' });
 //load env variables from docker-compose
 //const result = dotenv.config();
 
-=======
-const result = dotenv.config({ path: '/mnt/portfolio/paulgobero/configs/.env.prod' });
->>>>>>> .merge_file_a08428
 if (result.error) {
     throw result.error;
 }
