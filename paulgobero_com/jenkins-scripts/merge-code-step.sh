e3d945df5a0d9610f3222e4ecc16d2f75cbaee42
Paul G Lwanga <lwangapaul23@gmail.com>
2023-07-31 22:18:20 +0300

    Updated files to merge to production

--- paulgobero_com/jenkins-scripts/merge-code-step.sh
+++ paulgobero_com/jenkins-scripts/merge-code-step.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-###Script to merge prouction code into the Master branch###
+###Script to merge staging branch into prouction branch###
 
 #set git username and email
 git config --global user.name "server2"
@@ -9,9 +9,9 @@ echo The branches are:
 git branch
 git checkout -f production
 
-#If any of these files or folder changes in the development branch
-#merge them into the staging branch
-git checkout origin/staging paulgobero_com/models paulgobero_com/public paulgobero_com/routes paulgobero_com/views paulgobero_com/app.js paulgobero_com/Jenkinsfile paulgobero_com/package-lock.json paulgobero_com/package.json 
+#If any of these files or folder changes in the staging branch
+#merge them into the production branch
+git checkout origin/staging paulgobero_com/bin paulgobero_com/configs paulgobero_com/controllers paulgobero_com/models paulgobero_com/mongodb/Dockerfile paulgobero_com/public paulgobero_com/routes paulgobero_com/uploads paulgobero_com/utils paulgobero_com/views paulgobero_com/.dockerignore paulgobero_com/.gitignore paulgobero_com/app.js paulgobero_com/Jenkinsfile paulgobero_com/package-lock.json paulgobero_com/package.json paulgobero_com/wait-for 
 
 git status
 #git remote -v
