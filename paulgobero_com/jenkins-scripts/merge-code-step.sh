25121953d75d6a745022c20301f5ecc159cfa26e
paulmac <paul@paulgobero.com>
2023-07-31 23:26:06 +0300

    Updated files to merge into staging

--- paulgobero_com/jenkins-scripts/merge-code-step.sh
+++ paulgobero_com/jenkins-scripts/merge-code-step.sh
@@ -11,7 +11,7 @@ git checkout -f staging
 
 #If any of these files or folder changes in the development branch
 #merge them into the staging branch
-git checkout origin/development paulgobero_com/bin paulgobero_com/configs paulgobero_com/models paulgobero_com/mongodb/Dockerfile paulgobero_com/public paulgobero_com/routes paulgobero_com/controllers paulgobero_com/views paulgobero_com/app.js paulgobero_com/Jenkinsfile paulgobero_com/package-lock.json paulgobero_com/package.json paulgobero_com/.gitignore paulgobero_com/testdocker-compose.yml paulgobero_com/testdockerfile paulgobero_com/wait-for
+git checkout origin/development paulgobero_com/bin paulgobero_com/configs/loadb.js paulgobero_com/models paulgobero_com/mongodb/Dockerfile paulgobero_com/public paulgobero_com/routes paulgobero_com/controllers paulgobero_com/views paulgobero_com/app.js paulgobero_com/Jenkinsfile paulgobero_com/package-lock.json paulgobero_com/package.json paulgobero_com/.gitignore paulgobero_com/testdocker-compose.yml paulgobero_com/testdockerfile paulgobero_com/wait-for
 
 git status
 #git remote -v
