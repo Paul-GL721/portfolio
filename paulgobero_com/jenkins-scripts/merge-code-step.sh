7ec3b410d4a6713524606cde2de1f1b1f76c7975
Paul G Lwanga <lwangapaul23@gmail.com>
2023-08-03 17:18:55 +0300

    Updated merge folder path

--- paulgobero_com/jenkins-scripts/merge-code-step.sh
+++ paulgobero_com/jenkins-scripts/merge-code-step.sh
@@ -11,7 +11,7 @@ git checkout -f production
 
 #If any of these files or folder changes in the staging branch
 #merge them into the production branch
-git checkout origin/staging paulgobero_com/bin paulgobero_com/configs paulgobero_com/controllers paulgobero_com/models paulgobero_com/mongodb/Dockerfile paulgobero_com/public paulgobero_com/routes paulgobero_com/uploads paulgobero_com/utils paulgobero_com/views paulgobero_com/.dockerignore paulgobero_com/.gitignore paulgobero_com/app.js paulgobero_com/Jenkinsfile paulgobero_com/package-lock.json paulgobero_com/package.json paulgobero_com/wait-for 
+git checkout origin/staging $BASE_DIRECTORY/bin $BASE_DIRECTORY/configs $BASE_DIRECTORY/controllers $BASE_DIRECTORY/models $BASE_DIRECTORY/mongodb/Dockerfile $BASE_DIRECTORY/public $BASE_DIRECTORY/routes $BASE_DIRECTORY/uploads $BASE_DIRECTORY/utils $BASE_DIRECTORY/views $BASE_DIRECTORY/.dockerignore $BASE_DIRECTORY/.gitignore $BASE_DIRECTORY/app.js $BASE_DIRECTORY/Jenkinsfile $BASE_DIRECTORY/package-lock.json $BASE_DIRECTORY/package.json $BASE_DIRECTORY/wait-for 
 
 git status
 #git remote -v
