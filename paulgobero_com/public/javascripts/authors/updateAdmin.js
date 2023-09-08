3bde5bf0e27bbd2395c522a3743a63ae6158a0b8
server2 <lwangapaul23@gmail.com>
2023-09-08 18:43:04 +0000

    Merged from development branch: Build version 0.1.250

--- paulgobero_com/public/javascripts/authors/updateAdmin.js
+++ paulgobero_com/public/javascripts/authors/updateAdmin.js
@@ -31,4 +31,34 @@ $(document).ready(function(){
 			});
 		});
 	});
+
+	$(document).on("click", "#authordetailbtnEdit", function(){
+		//get value of second column
+		const updateid = $("#author_idvalue").text();
+		console.log('updated ')
+		$.ajax({
+			type: "GET",
+			url: "update",
+			data: { updateid:updateid },
+			success: function(data){
+				$("#authorUpdateModal").modal('show');					
+				$("#authorfirstname").val(data.name.first);
+				$("#authormiddlename").val(data.name.middle);
+				$("#authorlastname").val(data.name.last);
+				$("#authorshortdesc").val(data.about.short_description);
+				$("#authorfulldesc").val(data.about.full_description);
+				$("#authorbrandname").val(data.brandName);
+				$("#authoremail").val(data.email);
+				var originalgitUrl = data.socialmedia.github;
+				var originallinkUrl = data.socialmedia.linkedin;
+				$("#githuburl").val(originalgitUrl.replace(/&#x2F;/g, '/'));
+				$("#linkeninurl").val(originallinkUrl.replace(/&#x2F;/g, '/'));
+				$("#div1").attr("src", data.imageUrl);
+				$("#authorUpdateid").val(data._id);
+				$("#authorpassword").val(data.password);
+				$("#authorstatus").val(data.authorStatus);
+				$("#authorRole").val(data.authorRole);	
+			}
+		});
+	});
 });
