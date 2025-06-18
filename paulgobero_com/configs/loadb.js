9001aea6c6d67a5823dd17f7b3f339d674bc0877
server2 <lwangapaul23@gmail.com>
2025-06-18 17:06:51 +0000

    Merged from development branch: Build version 2.0.270

--- paulgobero_com/configs/loadb.js
+++ paulgobero_com/configs/loadb.js
@@ -18,7 +18,7 @@ const database_connection = async (db_name, db_user, db_passwd, db_host, db_port
 
    const env = process.env.NODE_ENV;
 
-   if (env === 'production' || env === 'stage' || env === 'test') {
+   if (env === 'production' || env === 'stage') {
       // Replica Set connection via Traefik in prod/staging/test
       mongoDBurl = `mongodb://${db_user}:${db_passwd}@${db_host}/${db_name}?authSource=admin&replicaSet=replicaset`;
    } else {
@@ -35,6 +35,7 @@ const database_connection = async (db_name, db_user, db_passwd, db_host, db_port
       return true; // Connection successful
    } catch (error) {
       console.error('FAILED TO CONNECT');
+      console.error('MongoDBurl is', mongoDBurl);
       console.error('Error connecting to MongoDB', error);
       return false; // Connection failed
    }
