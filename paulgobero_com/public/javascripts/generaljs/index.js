36f70e4d261b5416d5e531e6ced43255e61a8748
server2 <lwangapaul23@gmail.com>
2025-06-20 10:10:39 +0300

    fix(app): update the message post router
    update the message post router from '/'to '/portfolio' to remove the 404 error

--- paulgobero_com/public/javascripts/generaljs/index.js
+++ paulgobero_com/public/javascripts/generaljs/index.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
 
-  submitForm('#contactForm', '#contactmodal', '/' );
+  submitForm('#contactForm', '#contactmodal', '/portfolio' );
   //submitForm('#authorcreateid', '#createAuthormodal', 'create' );
 
   $('.project-modal-link').on('click', function() {
