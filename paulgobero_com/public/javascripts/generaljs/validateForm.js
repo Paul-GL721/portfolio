79ad48ab9865dbb513fcd7ac093b2200156a75be
server2 <lwangapaul23@gmail.com>
2025-06-27 13:27:25 +0300

    fix(form): different behavour if you submit without refreshing page
    re-bind the form validation after each form submission

--- paulgobero_com/public/javascripts/generaljs/validateForm.js
+++ paulgobero_com/public/javascripts/generaljs/validateForm.js
@@ -1,59 +1,53 @@
-$(document).ready(function() {
-	$("#contactForm").validate({
-		errorClass:"formValidationError",
-				
-		rules:{
-			contactname: {
-		      required: true,
-		      minlength: 3
-		    },
-		    contactemail: {
-		      required: true,
-		      email: true
-		    },
-		    contactmessage: {
-		      required: true,
-		      minlength: 3
-		    }
-		},
+function setupContactFormValidation() {
+  $("#contactForm").validate({
+    errorClass: "formValidationError",
+    
+    rules: {
+      contactname: { required: true, minlength: 3 },
+      contactemail: { required: true, email: true },
+      contactmessage: { required: true, minlength: 3 }
+    },
 
-		messages:{
-			contactname: {
-		      required: "I need your name to address you correctly",
-		      minlength: jQuery.validator.format("At least {3} characters required!")
-		    },
-		    contactemail: {
-		      required: "I need your email address to email you back",
-		      email: "Your email address must be in the format of name@domain.com"
-		    },
-		    contactmessage: {
-		      required: "Please type your message here",
-		      minlength: jQuery.validator.format("At least {3} characters required!")
-		    }
-		},
-		submitHandler: function(form) {
-			const formData = $(form).serialize(); 
-			console.log("This is the form data", formData);
+    messages: {
+      contactname: {
+        required: "I need your name to address you correctly",
+        minlength: jQuery.validator.format("At least {3} characters required!")
+      },
+      contactemail: {
+        required: "I need your email address to email you back",
+        email: "Your email address must be in the format of name@domain.com"
+      },
+      contactmessage: {
+        required: "Please type your message here",
+        minlength: jQuery.validator.format("At least {3} characters required!")
+      }
+    },
 
-			$.ajax({
-				url: "/portfolio",
-				method: "POST",
-				data: formData,  // serialized
-				dataType: 'json',
-				success: function(response) {
-					console.log("This is the data from the server",response);
-					
-					$('#contact_form_div').html(response.html); 
-					//$("#contactForm")[0].reset();
-					$('#contactmodal').modal('show'); 
-					//alert("Message sent successfuly"); 
-					//alert(response.success ? 'Message sent Successfully' : 'Message failed');
-				},
-				error: function(xhr, status, error) {
-					console.error(error);
-					alert('An error occurred');
-				}
-			});
-		}
-	});
-});
+    submitHandler: function(form) {
+      const formData = $(form).serialize();
+
+      $.ajax({
+        url: "/portfolio",
+        method: "POST",
+        data: formData,
+        dataType: "json",
+        success: function(response) {
+          $("#contact_form_div").html(response.html);
+          
+          //Re-bind validation on the new form
+          setupContactFormValidation();
+
+          alert("Message sent successfully");
+        },
+        error: function(xhr, status, error) {
+          console.error(error);
+          alert("An error occurred");
+        }
+      });
+
+      return false;
+    }
+  });
+}
+
+$(document).ready(setupContactFormValidation);
