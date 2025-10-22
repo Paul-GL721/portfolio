9744438a50e6732af2a1afd12c4e2a4280caa852
server2 <lwangapaul23@gmail.com>
2025-10-22 23:10:38 +0300

    fix(app): merge dev into staging

--- paulgobero_com/jenkins-scripts/merge-code-step.sh
+++ paulgobero_com/jenkins-scripts/merge-code-step.sh
@@ -13,6 +13,7 @@ git checkout -f staging
 #merge them into the staging branch
 git checkout origin/development $BASE_DIRECTORY/bin $BASE_DIRECTORY/configs/loadb.js $BASE_DIRECTORY/models $BASE_DIRECTORY/mongodb/Dockerfile $BASE_DIRECTORY/public $BASE_DIRECTORY/routes $BASE_DIRECTORY/controllers $BASE_DIRECTORY/views $BASE_DIRECTORY/app.js $BASE_DIRECTORY/Jenkinsfile $BASE_DIRECTORY/package-lock.json $BASE_DIRECTORY/package.json $BASE_DIRECTORY/.gitignore $BASE_DIRECTORY/testdocker-compose.yml $BASE_DIRECTORY/testdockerfile $BASE_DIRECTORY/wait-for $BASE_DIRECTORY/utils
 
+
 git status
 #git remote -v
 
